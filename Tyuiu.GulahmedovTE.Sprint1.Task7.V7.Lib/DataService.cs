3a6e6e84e4fd9cbe927ac9d2d3f4e8c2bd555de8
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-04 00:14:01 +0500

    Task7

---
+++ Tyuiu.GulahmedovTE.Sprint1.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.GulahmedovTE.Sprint1.Task7.V7.Lib
+{
+    public class DataService : ISprint1Task7V7
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = (Math.Cos(x)) / (Math.PI - 2 * Math.Pow(y, x)) + 16 * x * Math.Cos(x * y) - 2;
+            return Math.Round(res, 3);
+        }
+    }
+}
