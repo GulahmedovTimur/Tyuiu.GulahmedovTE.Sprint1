0c537e9451e937987d0c135f8de558e9891da0b4
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-04 00:05:09 +0500

    Task6

---
+++ Tyuiu.GulahmedovTE.Sprint1.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GulahmedovTE.Sprint1.Task6.V6.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint1.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string checkstr = "ребенок упал";
+            string waitstr = "ебенок пал";
+            var res = ds.DeleteFirstLetter(checkstr);
+            Assert.AreEqual(waitstr, res);
+
+        }
+    }
+}
